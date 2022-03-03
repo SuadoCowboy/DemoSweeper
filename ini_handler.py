dc72bf49bb7ba87026ca80fc211bbbea9edbe17e
Susado <77517394+SuadoCowboy@users.noreply.github.com>
2022-03-03 00:29:46 -0300

    .

--- ini_handler.py
+++ ini_handler.py
@@ -22,28 +22,36 @@ class config:
         
         if section not in self.sectionslist:
             self.sectionslist.append(section)
+        
+        i = 1
+        while config_name in self.configs:
+            config_name = config_name + '%IHSTART%' + str(i) + '%IHEND%'
         self.configs[config_name] = {'config':config, 'section':section, 'typeof':typeof}
         
     def getconfig(self):
         if os.path.exists(self.filename):
             self.cfg.read(self.filename)
             for config in self.configs:
+                config_name = config
+                if '%IHSTART%' in config and '%IHEND%' in config:
+                    config_name = config[:config.index('%IHSTART%')]
+                
                 if self.configs[config]['section'] not in self.cfg:
                     self.cfg[self.configs[config]['section']] = self.configs[config]['section']
                     with open(self.filename, 'w+') as configfile:
                         self.cfg.write(configfile)
-                if config not in self.cfg[self.configs[config]['section']]:
-                    self.cfg[self.configs[config]['section']][config] = str(self.configs[config]['config'])
+                if config_name not in self.cfg[self.configs[config]['section']]:
+                    self.cfg[self.configs[config]['section']][config_name] = str(self.configs[config]['config'])
                     with open(self.filename, 'w+') as configfile:
                         self.cfg.write(configfile)
                 if self.configs[config]['typeof'] == 'int':
-                    self.configs[config]['config'] = int(self.cfg[self.configs[config]['section']][config])
+                    self.configs[config]['config'] = int(self.cfg[self.configs[config]['section']][config_name])
                 if self.configs[config]['typeof'] == 'float':
-                    self.configs[config]['config'] = float(self.cfg[self.configs[config]['section']][config])
+                    self.configs[config]['config'] = float(self.cfg[self.configs[config]['section']][config_name])
                 if self.configs[config]['typeof'] == 'str':
-                    self.configs[config]['config'] = self.cfg[self.configs[config]['section']][config]
+                    self.configs[config]['config'] = self.cfg[self.configs[config]['section']][config_name]
                 if self.configs[config]['typeof'] == 'bool' or self.configs[config]['typeof'] == 'boolean':
-                    self.configs[config]['config'] = self.cfg[self.configs[config]['section']][config]
+                    self.configs[config]['config'] = self.cfg[self.configs[config]['section']][config_name]
                     if self.configs[config]['config'].lower() == 'true':
                         self.configs[config]['config'] = True
                     elif self.configs[config]['config'].lower() == 'false':
@@ -55,19 +63,31 @@ class config:
             for section in self.sectionslist:
                 self.cfg[section] = {}
             for config in self.configs:
-                self.cfg[self.configs[config]['section']][config] = str(self.configs[config]['config'])
+                config_name = config
+                if '%IHSTART%' in config and '%IHEND%' in config:
+                    config_name = config[:config.index('%IHSTART%')]
+                
+                self.cfg[self.configs[config]['section']][config_name] = str(self.configs[config]['config'])
             with open(self.filename, 'w+') as configfile:
                 self.cfg.write(configfile)
     def returnresult(self, singledict=False):
         result = {}
         if not singledict:
             for config in self.configs:
+                config_name = config
+                if '%IHSTART%' in config and '%IHEND%' in config:
+                    config_name = config[:config.index('%IHSTART%')]
+                
                 if self.configs[config]['section'] not in result:
                     result[self.configs[config]['section']] = {}
-                result[self.configs[config]['section']][config] = self.configs[config]['config']
+                result[self.configs[config]['section']][config_name] = self.configs[config]['config']
         else:
             for config in self.configs:
-                result[config] = self.configs[config]['config']
+                config_name = config
+                if '%IHSTART%' in config and '%IHEND%' in config:
+                    config_name = config[:config.index('%IHSTART%')]
+                
+                result[config_name] = self.configs[config]['config']
 
         return result
     def returneverything(self):
